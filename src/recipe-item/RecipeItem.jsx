31e2f3143736296548fe305b2e887bd744c952a7
Nika Donchenko <donchenko.veronika96@gmail.com>
2023-07-24 21:28:45 +0200

    trying to work with disputch, crashes with an error

--- src/recipe-item/RecipeItem.jsx
+++ src/recipe-item/RecipeItem.jsx
@@ -1,16 +1,26 @@
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import React from 'react'
 import styles from './RecipeItem.module.css'
+import { actions } from '../store/favorite/favorite.slice'
 
 
 const RecipeItem = ( {recipe} ) => {
     const { favorites } = useSelector(state => state)
+    console.log( favorites)
+
+    const dispatch = useDispatch()
+
+    const isExists = favorites.some(r => r.id === recipe.id)
 
     console.log(favorites)
   return (
     <div className={ styles.item }>
         <h2>{recipe.name}</h2>
-        <button>Add to favorites</button>
+        <button onClick = {() =>
+            dispatch(actions.
+                toggleFavorites(recipe))}>
+                {isExists ? 'Remove from' : 'Add to'} favorites
+            Add to favorites</button>
     </div>
   )
 }
--- src/store/favorite/favorite.slice.js
+++ src/store/favorite/favorite.slice.js
@@ -10,10 +10,13 @@ export const favoritesSlice = createSlice({
         toggleFavorites: (state, {payload: recipe}) => {
             const isExists = state.some(r => r.id === recipe.id)
             
-            if(isExists)
-             state = state.filter(r => r.id !== recipe.id)
-             else
-         state.push(recipe)
+            if(isExists){
+                const index = state.findIndex(item => item.id
+                 === recipe.id)
+            if (index !== -1) {
+                state.splice(index, 1)
+               } 
+            } else state.push(recipe)
         }
     }
 })
